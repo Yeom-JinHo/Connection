89ecf972561b9d605912f49122e9d759083ad48e
최진합 <pppooo95@naver.com>
2022-10-21 15:21:14 +0900

    refactor - 문제추천 페이지 탭 리팩토링

--- frontend/src/components/recommend/SideComponent.tsx
+++ frontend/src/components/recommend/SideComponent.tsx
@@ -5,18 +5,19 @@ import Tap from "./Tab";
 
 interface SideComponentProps {
   tabs: Tab[];
+  selectedTap: number;
   onTabClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
-function SideComponent({ tabs, onTabClick }: SideComponentProps) {
+function SideComponent({ tabs, selectedTap, onTabClick }: SideComponentProps) {
   return (
     <>
-      {tabs.map(tab => (
+      {tabs.map((tab, idx) => (
         <Tap
           key={tab.id}
           id={tab.id}
           label={tab.label}
-          isSelected={tab.isSelected}
+          isSelected={idx === selectedTap}
           onTabClick={onTabClick}
         />
       ))}
