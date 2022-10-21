f2f8120b71e4f16e785d68323028ec8c3dd8cfc1
Yeom-JinHo <pwlsghq@naver.com>
2022-10-21 17:45:00 +0900

    feat : 문제 풀이 시간설정 뷰

--- frontend/src/pages/StudyWith.tsx
+++ frontend/src/pages/StudyWith.tsx
@@ -1,9 +1,34 @@
+import { Center, CircularProgress } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { v4 } from "uuid";
 import NumberSetView from "../components/studyWith/NumberSetView";
+import ProblemSetView from "../components/studyWith/ProblemSetView";
+import TimeSetView from "../components/studyWith/TimeSetView";
 
 function StudyWith() {
   const [step, setStep] = useState(0);
-  return <NumberSetView onBtnClick={() => setStep(1)} />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isBoss, setIsBoss] = useState(false);
+
+  const bossView: React.FunctionComponentElement<undefined>[] = [
+    <NumberSetView key={v4()} onBtnClick={() => setStep(1)} />,
+    <ProblemSetView key={v4()} onBtnClick={() => setStep(2)} />,
+    <TimeSetView key={v4()} onBtnClick={() => setStep(3)} />
+  ];
+
+  return (
+    <Center>
+      {isLoading ? (
+        <CircularProgress size="120px" mt="30vh" isIndeterminate color="main" />
+      ) : (
+        <Center>
+          {bossView.map((view, ind) => {
+            return ind === step && view;
+          })}
+        </Center>
+      )}
+    </Center>
+  );
 }
 
 export default StudyWith;
