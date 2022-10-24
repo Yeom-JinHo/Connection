5936caf0ae1ba99603a9c31a97bdb855cc0a34db
김윤민 <k915k1004@naver.com>
2022-10-24 10:03:51 +0900

    style : 회원 가입 퍼블리싱

--- frontend/src/components/join/JoinModal.tsx
+++ frontend/src/components/join/JoinModal.tsx
@@ -1,9 +1,11 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
   Center,
   Flex,
   Input,
+  Link,
   ModalBody,
   ModalCloseButton,
   ModalContent,
@@ -16,59 +18,69 @@ function JoinModal() {
   return (
     <ModalContent bg="dep_1" maxW={650}>
       <ModalHeader />
-      <ModalCloseButton />
-      <ModalBody>
+      <ModalCloseButton size="10px" />
+      <ModalBody px="50px">
         <Text fontSize="30px" fontStyle="bold">
           회원가입
         </Text>
-        <Center py="60px" flexDir="column">
-          <Box flexDir="column" mb="50px">
-            <Box w="350px" flexDir="column">
-              <Box w="80px">
-                <Center mr="10px" h={10} fontSize="18px">
-                  백준ID
-                </Center>
-              </Box>
-              <Flex direction="column">
-                <Input type="text" />
-                <Text fontSize={12} mt="5px">
-                  연동하는데 문제가 발생했습니다
-                </Text>
-              </Flex>
-              <Box ml="10px">
-                <Button bg="gra">연동</Button>
-              </Box>
-            </Box>
-          </Box>
-          <Box flexDir="column">
-            <Flex w="350px">
-              <Box w="80px">
-                <Center mr="10px" h={10} fontSize="18px">
-                  Code
-                </Center>
-              </Box>
-              <Flex direction="column">
-                <Flex
-                  fontSize={20}
-                  h={10}
-                  fontWeight="bold"
-                  color="main"
-                  align="center"
-                >
-                  SD2SF4
-                </Flex>
-                <Text fontSize={12} mt="5px">
-                  백준 설정 {">"} 정보 수정 {">"} 상태메시지를
-                  <br /> Code로 변경한 뒤,
-                  <br /> <span style={{ color: "main" }}>연동</span> 버튼을
-                  눌러주세요
-                </Text>
-              </Flex>
-              <Box ml="10px">
-                <Button bg="gra">백준 이동</Button>
-              </Box>
+        <Center p="50px 0 30px" flexDir="column">
+          <Flex w="350px" mb="50px">
+            <Flex w="70px" h={10} fontSize="18px" alignItems="center">
+              백준ID
             </Flex>
-          </Box>
+            <Flex direction="column">
+              <Input type="text" />
+              <Text fontSize={12} mt="5px">
+                인증하는데 문제가 발생했습니다
+              </Text>
+            </Flex>
+            <Box ml="10px">
+              <Button bg="gra" _hover={{}}>
+                인증
+              </Button>
+            </Box>
+          </Flex>
+          <Flex w="350px">
+            <Flex w="70px" h={10} fontSize="18px" alignItems="center">
+              Code
+            </Flex>
+            <Flex direction="column">
+              <Flex
+                fontSize={20}
+                h={10}
+                fontWeight="bold"
+                color="main"
+                align="center"
+              >
+                SD2SF4
+              </Flex>
+              <Text fontSize={12} mt="5px">
+                백준 설정 {">"} 정보 수정 {">"} 상태메시지를
+                <br /> Code로 변경한 뒤,{" "}
+                <Text color="main" display="inline" fontWeight="bold">
+                  인증
+                </Text>{" "}
+                버튼을 눌러주세요
+              </Text>
+              <Link
+                href="https://www.acmicpc.net/"
+                isExternal
+                fontSize={12}
+                display="flex"
+                alignItems="center"
+                mt="10px"
+                textDecorationLine="underline"
+              >
+                백준으로 이동하기
+                <ExternalLinkIcon mx="2px" />
+              </Link>
+            </Flex>
+          </Flex>
+        </Center>
+        <Center mb="30px">
+          <Button bg="gra" width="200px" _hover={{}}>
+            가입하기
+          </Button>
         </Center>
       </ModalBody>
     </ModalContent>
