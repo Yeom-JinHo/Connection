a7d5c53275d29a00a2b3999bf2a38d703cc11eef
김윤민 <k915k1004@naver.com>
2022-10-24 16:17:58 +0900

    style : 스터디페이지 퍼블리싱

--- frontend/src/pages/Routes.tsx
+++ frontend/src/pages/Routes.tsx
@@ -9,6 +9,7 @@ import Recommend from "./Recommend";
 import StudyJoin from "./StudyJoin";
 import Header from "./Header";
 import Collection from "./study/Collecetion";
+import StudyTotal from "./study/StudyTotal";
 
 export default function Routes() {
   return (
@@ -17,7 +18,7 @@ export default function Routes() {
       <ReactRouterRoutes>
         <Route path="/" element={<div>main</div>} />
         <Route path="/recommend" element={<Recommend />} />
-        <Route path="/study">
+        <Route path="/study" element={<StudyTotal />}>
           <Route path="join" element={<StudyJoin />} />
           <Route path="collection" element={<Collection />} />
         </Route>
