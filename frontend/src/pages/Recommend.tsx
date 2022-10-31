e5bccd5728c5682bcf35b721616ecf1a714315c6
최진합 <pppooo95@naver.com>
2022-10-31 16:22:34 +0900

    feat - 문제집에 담기 / 삭제하기 기능 추가

--- frontend/src/pages/Recommend.tsx
+++ frontend/src/pages/Recommend.tsx
@@ -5,6 +5,7 @@ import ProblemCard from "../components/common/ProblemCard";
 import StudyLayout from "../components/layout/StudyLayout";
 import SideComponent from "../components/recommend/SideComponent";
 import { getRecommend } from "../api/problem";
+import { addWorkbook, deleteWorkbook, getWorkbook } from "../api/workbook";
 
 const TABS = [
   {
@@ -53,16 +54,51 @@ interface ProblemListProps {
 }
 
 function ProblemList({ problemList }: ProblemListProps) {
+  const [myWorkbook, setMyWorkbook] = useState<Problem[]>([]);
+  const [btnTypes, setBtnTypes] = useState(
+    problemList.map(problem =>
+      myWorkbook.findIndex(
+        p => p.problemInfo.problemId === problem.problemInfo.problemId
+      ) >= 0
+        ? "delete"
+        : "add"
+    )
+  );
+
+  const addProblem = async (problemId: number, idx: number) => {
+    const res = await addWorkbook(problemId);
+    const newBtnTypes = [...btnTypes];
+    newBtnTypes[idx] = "delete";
+    setBtnTypes(newBtnTypes);
+    console.log(problemId);
+  };
+  const deleteProblem = async (problemId: number, idx: number) => {
+    const res = await deleteWorkbook(problemId);
+    const newBtnTypes = [...btnTypes];
+    newBtnTypes[idx] = "add";
+    setBtnTypes(newBtnTypes);
+    console.log(problemId);
+  };
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getWorkbook();
+      setMyWorkbook(res.data);
+    };
+    fetch();
+  }, []);
+  // todo : 내 스터디 문제집에 있는 문제면 btnType을 delete로 바꾸고 onBtnClick핸들러도 문제집에서 삭제하는걸로 바꿔야됨
+  // todo : add 혹은 delete 마다 problemList 상태 업데이트 해야함.
   return (
     <Grid templateColumns="repeat(2,1fr)" gap="32px">
-      {problemList.map(problem => (
+      {problemList.map((problem, idx) => (
         <ProblemCard
           key={problem.problemInfo.problemId}
           problem={problem}
-          btnType="add"
+          btnType={btnTypes[idx]}
           onBtnClick={() => {
-            // 문제집에 문제 추가하기 API 요청
-            // addProblem(problem.id);
+            if (btnTypes[idx] === "add")
+              addProblem(problem.problemInfo.problemId, idx);
+            else deleteProblem(problem.problemInfo.problemId, idx);
           }}
         />
       ))}
@@ -73,11 +109,6 @@ function ProblemList({ problemList }: ProblemListProps) {
 function Recommend() {
   const [recommends, setRecommends] = useState<null | RecommendsType>(null);
   const [selectedTap, setSelectedTap] = useState(0);
-
-  const addProblem = (problemId: number) => {
-    // todo : 문제집에 넣을지 확인하고 문제집에 넣을 로직 구현 필요
-    console.log(problemId);
-  };
 
   const onTabClick: React.MouseEventHandler<HTMLDivElement> = e => {
     if (e.target instanceof HTMLDivElement && e.target.dataset.id) {
