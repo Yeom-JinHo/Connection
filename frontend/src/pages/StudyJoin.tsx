6d6938f3cc5a7de3e18a7e8667cbb5b2c7ae85e2
Yeom-JinHo <pwlsghq@naver.com>
2022-11-02 14:15:50 +0900

    feat : 스터디 참가로직

--- frontend/src/pages/StudyJoin.tsx
+++ frontend/src/pages/StudyJoin.tsx
@@ -1,11 +1,46 @@
 import { Accordion, Center, useDisclosure } from "@chakra-ui/react";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { createStudy, getStudyInfo, GetStudyInfoRes } from "../api/studyJoin";
 import StudyInfoModal from "../components/join/StudyInfoModal";
 import JoinAccordionItem from "../components/study/JoinAccordionItem";
 
+type ErrorMsgType =
+  | ""
+  | "유효하지 않은 스터디 코드입니다."
+  | "이미 존재하는 스터디명입니다."
+  | "스터디코드를 입력해주세요.";
+
 function StudyJoin() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [studyName, setStudyName] = useState("");
+  const [studyCode, setStudyCode] = useState("");
+  const [studyInfo, setStudyInfo] = useState<GetStudyInfoRes>(
+    {} as GetStudyInfoRes
+  );
+  const [createErr, setCreateErr] = useState<ErrorMsgType>("");
+  const [joinErr, setJoinErr] = useState<ErrorMsgType>("");
 
+  const handleCreateBtn = async () => {
+    const res = await createStudy("123");
+    return null;
+  };
+
+  const handleJoinBtn = async () => {
+    if (!studyCode) {
+      setJoinErr("스터디코드를 입력해주세요.");
+      return;
+    }
+
+    const res = await getStudyInfo(studyCode);
+    if (axios.isAxiosError(res)) {
+      setJoinErr("유효하지 않은 스터디 코드입니다.");
+    } else {
+      setJoinErr("");
+      setStudyInfo(res.data);
+      onOpen();
+    }
+  };
   return (
     <Center w="1200px" m="auto">
       <Accordion w="640px" mt="160px" allowToggle>
@@ -13,20 +48,20 @@ function StudyJoin() {
           title="스터디 생성하기"
           panelTitle="스터디명"
           btnTitle="생성"
-          errMsg=""
-          onInputChange={undefined}
-          onBtnClick={undefined}
+          errMsg={createErr}
+          onInputChange={(name: string) => setStudyName(name)}
+          onBtnClick={handleCreateBtn}
         />
         <JoinAccordionItem
           title="스터디 참가하기"
           panelTitle="스터디 코드"
           btnTitle="참가"
-          errMsg="유효하지 않은 스터디 코드입니다."
-          onInputChange={undefined}
-          onBtnClick={onOpen}
+          errMsg={joinErr}
+          onInputChange={(code: string) => setStudyCode(code)}
+          onBtnClick={handleJoinBtn}
         />
       </Accordion>
-      <StudyInfoModal isOpen={isOpen} onClose={onClose} />
+      <StudyInfoModal studyInfo={studyInfo} isOpen={isOpen} onClose={onClose} />
     </Center>
   );
 }
