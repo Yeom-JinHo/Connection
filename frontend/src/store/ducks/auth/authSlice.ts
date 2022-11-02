d17981a6847969fcc03a557a9f565fb0ee09d886
김윤민 <k915k1004@naver.com>
2022-11-02 15:25:42 +0900

    feat : redux 수정

--- frontend/src/store/ducks/auth/authSlice.ts
+++ frontend/src/store/ducks/auth/authSlice.ts
@@ -13,27 +13,21 @@ export const authSlice = createSlice({
   reducers: {
     setUserInfo: (state, action) => {
       state.check = true;
-      state.information = action.payload;
+      state.information = { ...action.payload };
+    },
+    updateUserInfo: (state, action) => {
+      state.information = { ...state.information, ...action.payload };
     },
     resetUserInfo: state => {
       state.check = false;
       state.information = null;
       localStorage.removeItem("token");
     }
-    // setMoreInfo: (state, action) => {
-    //   if (state.information) {
-    //     state.information.area = action.payload.area;
-    //     state.information.categorys = action.payload.categorys;
-    //   }
-    // }
   },
   extraReducers: builder => {
     builder.addCase(getUserInfo.fulfilled, (state, { payload }) => {
       state.check = true;
       state.information = { ...payload };
-    });
-    builder.addCase(getUserMorInfo.fulfilled, (state, { payload }) => {
-      state.information = { ...state.information, ...payload };
     });
   }
 });
