a400a9aab1a5bffe5278b85a3e88f67fd48b57ba
최진합 <pppooo95@naver.com>
2022-11-03 14:04:43 +0900

    feat- 많이 안푼유형 문제 추천 추가

--- frontend/src/pages/Recommend.tsx
+++ frontend/src/pages/Recommend.tsx
@@ -11,12 +11,20 @@ const TABS = [
   {
     id: 0,
     label: "많이 푼 문제",
-    msg: "자주 출제되는 유형 중/다른 사람들이 많이 푼 문제들을 추천해 줄게요. "
+    msg: "자주 출제되는 유형 중/다른 사람들이 많이 푼 문제들을 추천해 줄게요. ",
+    category: "popular"
   },
   {
     id: 1,
     label: "많이 담은 문제",
-    msg: "어떤 문제를 같이 풀지 모르겠어요?/다른 스터디가 문제집에 담아놓은 문제들을 추천해 줄게요. "
+    msg: "어떤 문제를 같이 풀지 모르겠어요?/다른 스터디가 문제집에 담아놓은 문제들을 추천해 줄게요. ",
+    category: "workbook"
+  },
+  {
+    id: 2,
+    label: "많이 안 푼 유형",
+    msg: "자주 출제되는 유형 중/많이 안 푼 유형의 문제들을 추천해 줄게요. ",
+    category: "weak"
   }
 ];
 
@@ -47,6 +55,7 @@ export interface Problem {
 interface RecommendsType {
   popular: Problem[];
   workbook: Problem[];
+  weak: Problem[];
 }
 
 interface ProblemListProps {
@@ -141,7 +150,7 @@ function Recommend() {
       {recommends && (
         <ProblemList
           problemList={
-            selectedTap === 0 ? recommends?.popular : recommends?.workbook
+            recommends[TABS[selectedTap].category as keyof RecommendsType]
           }
         />
       )}
