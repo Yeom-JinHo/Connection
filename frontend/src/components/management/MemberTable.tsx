726cf1a2bbabcf24ffc31f4ca928fb3cbcdd38cb
최진합 <pppooo95@naver.com>
2022-11-10 15:32:26 +0900

    fix - 카드 보더,섀도우, 이미지 보더

--- frontend/src/components/management/MemberTable.tsx
+++ frontend/src/components/management/MemberTable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, Grid, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, Image, Text } from "@chakra-ui/react";
 import ReactApexChart from "react-apexcharts";
 
 import useChartOption from "../../hooks/useChartOption";
@@ -18,9 +18,14 @@ function MemberTable({ members, onBanBtnClick, isBoss }: MemberTableProps) {
   return (
     <Grid templateColumns="repeat(2,1fr)" gap="32px">
       {members.map(member => (
-        <Box bg="dep_1" key={member.userId}>
-          <Flex bg="dep_2" p={2} textAlign="center">
-            <img
+        <Box
+          bg="dep_1"
+          key={member.userId}
+          borderRadius="20px"
+          shadow="2px 4px 4px rgba(0, 0, 0, 0.25)"
+        >
+          <Flex bg="dep_2" p={2} textAlign="center" borderTopRadius="20px">
+            <Image
               src={member.imageUrl}
               alt={member.name}
               width="24px"
@@ -28,6 +33,7 @@ function MemberTable({ members, onBanBtnClick, isBoss }: MemberTableProps) {
                 e.currentTarget.src =
                   "https://avatars.githubusercontent.com/u/48246705?s=40&v=4";
               }}
+              borderRadius="50%"
             />
             <Text flexGrow={3}>{member.name}</Text>
             {isBoss && member.userId !== auth.information.userId && (
